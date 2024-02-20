aa702ba99ff193e693c5d1b47a554787216303fb
nan <nan>
2024-02-20 14:21:32 +0530

    update urls

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ toggles.forEach((toggle) => {
   toggle.addEventListener("click", () => {
     toggle.nextElementSibling.classList.toggle("expanded");
     if (toggle.children[0].getAttribute("src").endsWith("icon-plus.svg")) {
-      toggle.children[0].setAttribute("src", "/assets/images/icon-minus.svg");
+      toggle.children[0].setAttribute("src", "./assets/images/icon-minus.svg");
     } else {
-      toggle.children[0].setAttribute("src", "/assets/images/icon-plus.svg");
+      toggle.children[0].setAttribute("src", "./assets/images/icon-plus.svg");
     }
   });
 });
